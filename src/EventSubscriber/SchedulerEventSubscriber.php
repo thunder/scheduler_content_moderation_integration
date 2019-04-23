495fdbfab9bb3b33a2419fc3e9aa8819dc03c4fd
Volker Killesreiter <killesre@gmail.com>
2019-04-23 17:36:25 +0200

    implement new scheduler hooks

---
+++ src/EventSubscriber/SchedulerEventSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\content_moderation\EventSubscriber;
+
+namespace Drupal\scheduler_api_test;
+
+use Drupal\scheduler\SchedulerEvent;
+use Drupal\scheduler\SchedulerEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Handle scheduler events..
+ */
+class SchedulerEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[SchedulerEvents::PUBLISH_IMMEDIATELY][] = ['publishImmediately'];
+    return $events;
+  }
+
+  /**
+   * Operations before Scheduler publishes a node immediately not via cron.
+   *
+   * @param \Drupal\scheduler\SchedulerEvent $event
+   *   The scheduler event.
+   */
+  public function publishImmediately(SchedulerEvent $event) {
+    /** @var \Drupal\node\Entity\Node $node */
+    $node = $event->getNode();
+    $node->set('moderation_state', $node->publish_state->getValue());
+
+    $event->setNode($node);
+  }
+
+}
