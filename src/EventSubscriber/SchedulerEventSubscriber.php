5f73d5eaa1462c1ca643acc2e777595a14a9d5e0
Volker Killesreiter <killesre@gmail.com>
2019-04-24 09:57:18 +0200

    align to https://www.drupal.org/i/3049346

--- src/EventSubscriber/SchedulerEventSubscriber.php
+++ src/EventSubscriber/SchedulerEventSubscriber.php
@@ -7,23 +7,25 @@ use Drupal\scheduler\SchedulerEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
- * Handle scheduler events..
+ * Handle scheduler events.
+ *
+ * The six possible Scheduler events are:
+ * SchedulerEvents::PRE_PUBLISH
+ * SchedulerEvents::PUBLISH
+ * SchedulerEvents::PRE_UNPUBLISH
+ * SchedulerEvents::UNPUBLISH
+ * SchedulerEvents::PRE_PUBLISH_IMMEDIATELY
+ * SchedulerEvents::PUBLISH_IMMEDIATELY.
  */
 class SchedulerEventSubscriber implements EventSubscriberInterface {
 
   /**
-   * {@inheritdoc}
-   */
-  public static function getSubscribedEvents() {
-    $events[SchedulerEvents::PUBLISH_IMMEDIATELY][] = ['publishImmediately'];
-    return $events;
-  }
-
-  /**
-   * Operations before Scheduler publishes a node immediately not via cron.
+   * Operations to perform after Scheduler publishes a node immediately.
+   *
+   * This is during the edit process, not via cron.
    *
    * @param \Drupal\scheduler\SchedulerEvent $event
-   *   The scheduler event.
+   *   The event being acted on.
    */
   public function publishImmediately(SchedulerEvent $event) {
     /** @var \Drupal\node\Entity\Node $node */
@@ -31,6 +33,15 @@ class SchedulerEventSubscriber implements EventSubscriberInterface {
     $node->set('moderation_state', $node->publish_state->getValue());
 
     $event->setNode($node);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    // The values in the arrays give the function names above.
+    $events[SchedulerEvents::PUBLISH_IMMEDIATELY][] = ['publishImmediately'];
+    return $events;
   }
 
 }
